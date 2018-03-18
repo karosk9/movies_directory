5f4eb4a3818407186ac620dbe64e18d69ddd0825
Karolina <karosk9@gmail.com>
2018-03-18 14:11:39 +0100

    create admins

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
-
+  has_many :reviews
+  
   def movie_types=(text)
     text.is_a?(String) ? super(sanitize_movie_types(text)) : super(text)
   # jeśli text jest stringiem wywołuje metodę sanitize tags która zmienia string na array, jeśli nie jest stringiem nie wykona się
